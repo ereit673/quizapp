2af638adcd3e9022da7289db06d6f2dd4a19c3c4
Alex Eremie <alex.eremie19@gmail.com>
2024-02-15 07:05:12 +0100

    added JSONs and styled the question container

---
+++ script.js
@@ -0,0 +1,34 @@
+let questions = [
+    {
+        "question": "What is the capital of Australia?",
+        "answer_1": "Sydney",
+        "answer_2": "Canberra",
+        "answer_3": "Melbourne",
+        "answer_4": "Brisbane",
+        "right_answer": 2,
+    },
+    {
+        "question": "Which mountain range is known as the 'Roof of the World'?",
+        "answer_1": "Andes",
+        "answer_2": "Alps",
+        "answer_3": "Himalayas",
+        "answer_4": "Rockies",
+        "right_answer": 3
+    },
+    {
+        "question": "Which ocean is the largest by surface area?",
+        "answer_1": "Atlantic Ocean",
+        "answer_2": "Indian Ocean",
+        "answer_3": "Southern Ocean",
+        "answer_4": "Pacific Ocean",
+        "right_answer": 4
+      },
+      {
+        "question": "In which continent is the country of Egypt located?",
+        "answer_1": "Asia",
+        "answer_2": "Africa",
+        "answer_3": "Europe",
+        "answer_4": "South America",
+        "right_answer": 2
+      }
+];
