bc2b01a3c75b4f1ed6148c014c743906f2996eae
Alex Eremie <alex.eremie19@gmail.com>
2024-02-15 07:45:37 +0100

    added functions showQuestion and answer

--- script.js
+++ script.js
@@ -32,3 +32,29 @@ let questions = [
         "right_answer": 2
       }
 ];
+
+let currentQuestion = 0;
+
+function init(){
+    showQuestion();
+}
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+    
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection){
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    if(selectedQuestionNumber == question['right_answer']){
+        console.log('Richtige Antwort!')
+    } else {
+        console.log('Falsche Antwort!')
+    };
+}
